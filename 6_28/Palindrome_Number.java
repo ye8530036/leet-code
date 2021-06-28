efe238b629a4a655340fd3fb6024b2369a89bbb5
yejiayu <877271720@qq.com>
2021-06-28 15:53:11 +0800

    基础代码提交

---
+++ 6_28/Palindrome_Number.java
@@ -0,0 +1,15 @@
+//回文数
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
+        int res = 0;
+        int ret = x;
+        while(ret!=0) {
+            //每次取末尾数字
+            int tmp = ret%10;
+            res = res*10 + tmp;
+            ret /= 10;
+        }
+         return res == x;
+    }
+}
