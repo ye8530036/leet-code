734b5fb382346fbba4ac0b33dd5d948c0b0c086d
yejiayu <877271720@qq.com>
2021-07-12 14:15:18 +0800

    7-12

---
+++ test/Length_of_Last_Word.java
@@ -0,0 +1,20 @@
+/**
+ * 最后一个单词的长度
+ * 7-12
+ * 给你一个字符串 s，由若干单词组成，单词之间用空格隔开。返回字符串中最后一个单词的长度。如果不存在最后一个单词，请返回 0 。
+ * 单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ * 字符串遍历（倒序遍历）
+ */
+class Length_of_Last_Word {
+    public int lengthOfLastWord(String s) {
+        int end=s.length()-1;
+        while (end>=0 && s.charAt(end)==' ')
+            end --;
+        if (end<0)
+            return 0;
+        int start =end;
+        while (start>=0 && s.charAt(start)!=' ')
+            start --;
+        return end-start;
+    }
+}
