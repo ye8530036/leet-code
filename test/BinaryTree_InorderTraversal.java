30abb0fcb74443c764886b152252d165839a7a5b
yejiayu <877271720@qq.com>
2021-07-27 17:05:24 +0800

    7-27

---
+++ test/BinaryTree_InorderTraversal.java
@@ -0,0 +1,51 @@
+/**
+ * 二叉树的中序遍历
+ * 7-27
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ * 递归(先递归左节点，再根节点)
+ *
+ * 前序遍历：打印 - 左 - 右
+ * 中序遍历：左 - 打印 - 右
+ * 后序遍历：左 - 右 - 打印
+ *
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class BinaryTree_InorderTraversal {
+
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        dfs(res,root);
+        return res;
+    }
+
+    void dfs(List<Integer> res,TreeNode root){
+        if (root == null){return;}
+        dfs(res,root.left);
+        res.add(root.val);
+        dfs(res,root.right);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
