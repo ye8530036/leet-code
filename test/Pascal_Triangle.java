8c891c41d196aba64b80f6607f9bb61fac5a9b6c
yejiayu <877271720@qq.com>
2021-08-06 09:55:34 +0800

    8-5

---
+++ test/Pascal_Triangle.java
@@ -0,0 +1,31 @@
+/**
+ * 杨辉三角
+ * ８-6
+ * 给定一个非负整数 numRows，生成「杨辉三角」的前 numRows 行。
+ * 在「杨辉三角」中，每个数是它左上方和右上方的数的和。
+ *
+ * 思路及算法:
+ * 公式:C(i, j) = C(i - 1, j - 1) + C(i - 1, j)
+ */
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Pascal_Triangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> temp = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    temp.add(1);
+                } else {
+                    temp.add(res.get(i - 1).get(j - 1) + res.get(i - 1).get(j));
+                }
+            }
+            res.add(temp);
+        }
+        return res;
+    }
+}
