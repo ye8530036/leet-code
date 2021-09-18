a9a7368e4071775bd0187017bcb8a2143b09ee3c
ye8530036 <877271720@qq.com>
2021-09-18 18:15:25 +0800

    9-18

--- test/IsIsomorphic.java
+++ test/IsIsomorphic.java
@@ -19,7 +19,7 @@ class IsIsomorphic {
         for(int i=0;i<s.length();i++) {
             if (s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))) {
                 return false;
-            }
+             }
         }
         return true;
     }
