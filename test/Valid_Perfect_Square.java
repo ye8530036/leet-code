38afa0fd10b18c1ecd06706d69b15e14c6f8eeda
ye8530036 <877271720@qq.com>
2021-11-04 17:06:21 +0800

    11-4

--- test/Valid_Perfect_Square.java
+++ test/Valid_Perfect_Square.java
@@ -10,14 +10,17 @@ public class Valid_Perfect_Square {
     public boolean isPerfectSquare(int num) {
         int left=0,right = num;
         while (left<=right){
+            //定义中间值
             int mid = (right-left)/2+left;
             long square = (long) mid*mid;
+            //验证mid是否是平方数
             if (square<num){
                 left=mid+1;
             }
             else if (square>num) {
                 right=mid-1;
             }
+
             else return true;
         }
         return false;
