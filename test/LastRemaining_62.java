3a4cab4ccccb6fd513a34d894c7429cdbbf89be6
叶嘉宇 <877271720@qq.com>
2022-02-22 13:17:34 +0800

    2-21

---
+++ test/LastRemaining_62.java
@@ -0,0 +1,16 @@
+/**
+ * 题目：约瑟夫环问题
+ * 时间：2022.2.22
+ * 算法公式：(当前index + m) % 上一轮剩余数字的个数
+ */
+
+class LastRemaining_62 {
+    public int lastRemaining(int n, int m) {
+        int ans=0;
+        //从最后开始反推最后仅剩下2个，所以从两个数开始向上加
+        for (int i=2;i<=n;i++){
+            ans = (ans+m)%i;
+        }
+        return ans;
+    }
+}
