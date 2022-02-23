1ec10f3931cc278a97b736bc9abb10692a0dc352
叶嘉宇 <877271720@qq.com>
2022-02-23 21:25:02 +0800

    2-23

---
+++ test/ReverseOnlyLetters_917.java
@@ -0,0 +1,16 @@
+class ReverseOnlyLetters_917 {
+    public String reverseOnlyLetters(String s) {
+        char[] cs = s.toCharArray();
+        int n = cs.length;
+        for (int i=0,j=n-1;i<j;){
+            while (i<j&& !Character.isLetter(cs[i]))i++;
+            while (i<j&& !Character.isLetter(cs[j]))j--;
+            if (i<j){
+                char c  = cs[i];
+                cs[i] = cs[j];
+                cs[j] = c;
+            }
+        }
+        return String.valueOf(cs);
+    }
+}
