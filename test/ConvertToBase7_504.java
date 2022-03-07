56906577a8e01c005bc21c052a9700019b8418a5
叶嘉宇 <877271720@qq.com>
2022-03-07 13:08:40 +0800

    2022-3-7

--- test/ConvertToBase7_504.java
+++ test/ConvertToBase7_504.java
@@ -9,7 +9,19 @@
 
 class ConvertToBase7_504 {
     public String convertToBase7(int num) {
-        //转换成7进制
-        return Integer.toString(num,7);
+        //（1）直接调用借口，转换7进制
+      //  return Integer.toString(num,7);
+        StringBuilder sb = new StringBuilder();
+        if (num<0){
+            return '-'+convertToBase7(-num);
+        }
+        if (num==0){
+            return "0";
+        }
+       while (num!=0){
+            sb.append(num%7);
+            num /=7;
+        }
+        return sb.reverse().toString();
     }
 }
